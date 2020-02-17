a7ff4ccdb746863a7a5b8dd95059ab923bbd73b1
Tenclar <tenclarvalus@gmail.com>
2020-02-16 23:46:41 -0500

    api fastfeet

---
+++ api/src/app.js
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+import './database';
+import express from 'express';
+import 'express-async-errors';
+import Youch from 'youch';
+import cors from 'cors';
+import path from 'path';
+
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
+
+  middlewares() {
+    this.server.use(cors());
+    this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal Server Error' });
+    });
+  }
+}
+export default new App().server;
---
+++ api/src/routes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.get('/', (req, res) => {
+  return res.json({ message: ' api ok' });
+});
+routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+routes.get('/users', UserController.index);
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
