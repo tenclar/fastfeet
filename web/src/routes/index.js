771894d587705c60d714486776a99be8fd987570
Tenclar <tenclarvalus@gmail.com>
2020-03-28 16:37:45 -0500

    styles

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -3,9 +3,12 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
+import DeliveryManForm from '../pages/DeliveryMan/Form';
 import DeliveryMan from '../pages/DeliveryMan';
+import RecipientForm from '../pages/Recipient/Form';
 import Recipient from '../pages/Recipient';
 import Order from '../pages/Order';
+import OrderForm from '../pages/Order/Form';
 import Profile from '../pages/Profile';
 
 import Problem from '../pages/Problem';
@@ -15,11 +18,30 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
-      <Route path="/deliveryman" component={DeliveryMan} isPrivate />
-      <Route path="/recipient" component={Recipient} isPrivate />
-      <Route path="/order" component={Order} isPrivate />
+
+      <Route
+        path="/entregadores/editar/:id"
+        component={DeliveryManForm}
+        isPrivate
+      />
+      <Route path="/entregadores/novo" component={DeliveryManForm} isPrivate />
+      <Route path="/entregadores" component={DeliveryMan} isPrivate />
+
+      <Route
+        path="/destinatarios/editar/:id"
+        component={RecipientForm}
+        isPrivate
+      />
+      <Route path="/destinatarios/novo" component={RecipientForm} isPrivate />
+      <Route path="/destinatarios" component={Recipient} isPrivate />
+
+      <Route path="/encomendas/editar/:id" component={OrderForm} isPrivate />
+      <Route path="/encomendas/novo" component={OrderForm} isPrivate />
+      <Route path="/encomendas" component={Order} isPrivate />
+
+      <Route path="/entregas/problemas" component={Problem} isPrivate />
       <Route path="/profile" component={Profile} isPrivate />
-      <Route path="/problems" component={Problem} isPrivate />
+
       <Route path="/" component={() => <h1>404</h1>} />
     </Switch>
   );
--- web/src/styles/Title/index.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Title = styled.div`
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 20px;
-`;
-export default Title;
